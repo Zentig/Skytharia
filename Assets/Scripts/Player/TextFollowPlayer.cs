d2faf2d76a3aaabe222362525f4a2f5f21d8a2ef
F1nndegamer <140960106+F1nndegamer@users.noreply.github.com>
2023-11-04 11:14:01 +0100

    Items
    i have added item you know

--- Assets/Scripts/Player/TextFollowPlayer.cs
+++ Assets/Scripts/Player/TextFollowPlayer.cs
@@ -9,7 +9,7 @@ public class TextFollowPlayer : MonoBehaviour
         if (player != null)
         {
             // Use the position of the player to position the text above their head
-            transform.position = player.position + Vector3.up * 1.5f; // Adjust the Vector3.up value for the desired height.
+            transform.position = player.position + Vector3.up * 1.8f; // Adjust the Vector3.up value for the desired height.
         }
     }
 }
